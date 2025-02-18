89554597b4e3e27f569f2620e00c01541ea7e901
AndrewBSC1 <andrinho9907@gmail.com>
2025-02-18 12:20:02 -0300

    correção

--- app.js
+++ app.js
@@ -1,76 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-// CAPTURA ELEMENTOS
-let amigo = document.getElementById("nome-amigo"); //input
-let listaAmigos = document.getElementById("lista-amigos"); // paragrafo
-let listaSorteio = document.getElementById("lista-sorteio"); // paragrafo
+let elemAmigo = document.getElementById("nome-amigo"); //input
+let elemListaAmigos = document.getElementById("lista-amigos"); // paragrafo
+let elemListaSorteio = document.getElementById("lista-sorteio"); // paragrafo
 
-// LISTA DE ARRAYS
-let amigosIncluidos = [];
-let amigosSorteados = [];
+let amigos = [];
 
-// Contador de tentativa de embaralhamento
-let contador = 1;
-
-// FUNCAO ADICIONAR AMIGO
 function adicionar() {
   // Adiciona participante na lista de amigos desde que o campo não esteja em vazio ou já adicionado
   const amigo = elemAmigo.value;
-  if (amigo && !arrAmigosIncluidos.includes(amigo.toUpperCase())) {
-    arrAmigosIncluidos.push(amigo.toUpperCase());
-    elemListaAmigos.textContent = arrAmigosIncluidos.join(", ");
+  if (amigo && !amigos.includes(amigo.toUpperCase())) {
+    amigos.push(amigo.toUpperCase());
+    elemListaAmigos.textContent = amigos.join(", ");
     elemAmigo.value = "";
   }
 }
-
 function sortear() {
-  // Limpa lista de sorteios
-  listaSorteio.innerHTML = "";
+  elemListaSorteio.innerHTML = "";
 
-  const sorteioBemSucedido = realizaSorteio();
-
-  if (sorteioBemSucedido) {
-    amigosIncluidos.forEach((item, i) => {
-      elemListaSorteio.innerHTML += `${item} --> ${arrAmigosSorteados[i]}<br>`;
-    });
-    console.log("Amigos sorteados", amigosSorteados);
-    contador = 1;
-  } else {
-    contador += 1;
-    sortear();
-  }
-}
-
-// FUNCAO PARA GERAR LISTA DE AMIGO E SEU AMIGO SECRETO
-function realizaSorteio() {
-  amigosSorteados.length = 0;
-
-  const amigosEmbaralhados = amigosIncluidos.slice();
-  embaralhaAmigos(amigosEmbaralhados);
-  console.log(`Embaralhamento n: ${contador}`, amigosEmbaralhados);
-
-  // Verifica se o amigo tirou ele mesmo e retorna o status falso ou verdadeiro
-  for (let i = 0; i < amigosIncluidos.length; i++) {
-    if (amigosEmbaralhados[i] == amigosIncluidos[i]) {
-      return false;
+  embaralhaArray(amigos);
+  for (i = 0; i < amigos.length; i++) {
+    if (i == amigos.length - 1) {
+      elemListaSorteio.innerHTML += `${amigos[i]} --> ${amigos[0]}<br>`;
+    } else {
+      elemListaSorteio.innerHTML += `${amigos[i]} --> ${amigos[i + 1]}<br>`;
     }
-    // Se não tiver erro, adiciona na lista de sorteados
-    amigosSorteados.push(amigosEmbaralhados[i]);
   }
-  return true;
 }
 
-// FUNCAO PARA EMBARALHAR LISTA DE AMIGOS
-function embaralhaAmigos(arr) {
+// FUNCAO PARA EMBARALHAR ARRAY
+function embaralhaArray(arr) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
 }
 
-// FUNCAO DE REINICIAR
 function reiniciar() {
-  amigosIncluidos.length = 0;
-  listaSorteio.innerHTML = "";
-  amigo.value = "";
-  listaAmigos.textContent = "";
+  amigos.length = 0;
+  elemListaSorteio.innerHTML = "";
+  elemAmigo.value = "";
+  elemListaAmigos.textContent = "";
 }
